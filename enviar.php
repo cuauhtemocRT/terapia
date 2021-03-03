5bfcab7d8a352248443c12af5eea85eb2285f408
Cuauhtemoc Rodriguez <1979cuau@gmail.com>
2021-03-03 02:59:25 -0600

    Se configura archivo de envío de correo

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Terapia que solicita: " . $subject . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = '1979cuau@gmail.com';
+$asunto = 'Mensaje de sitio web';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
